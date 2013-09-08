edeacac79e159560d9a176e0b33e17af03b5c5d2
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-09-08 15:34:02 -0300

    Melhorias na jogabilidade

--- client/jogo-da-velha.js
+++ client/jogo-da-velha.js
@@ -1,15 +1,39 @@
+WEAPON_X = 'icon-x', WEAPON_O = 'icon-o';
 Users = new Meteor.Collection('users');
+GameStream = new Meteor.Stream('game');
 
 Meteor.startup(function() {
 	Meteor.subscribe('onlines');
-	Session.set('weapon', 'icon-x');
+  $('.input').focus();
 });
+
+var resetSession = function(room) {
+  if(Session.equals('room', room)) {
+    Session.set('enemy', null);
+    Session.set('room', null);
+    Session.set('weapon', WEAPON_X);
+  }
+}
 
 Template.onlines.events({
   "submit .form": function(event) {
     var name = $(event.target).find('.input').val();
     Session.set('user', name);
-    Users.insert({name: name});
+    Session.set('weapon', WEAPON_X);
+    GameStream.emit('enter', name);
+    event.preventDefault();
+  },
+  "click .play": function(event) {
+    var user = Session.get('user');
+    if(user) {
+      var enemy = $(event.target).data('enemy');
+      var room = Random.id();
+      Session.set('room', room);
+      GameStream.emit('invite', enemy, user, room);
+    } else {
+      alert('Digite seu nome primeiro.');
+      $('.input').focus();
+    }
     event.preventDefault();
   }
 });
@@ -22,20 +46,86 @@ Template.onlines.users = function() {
 	return Users.find({name: {'$ne': Session.get('user')}});
 };
 
+Template.onlines.hasRoom = function() {
+  return Session.get('room');
+};
+
+Template.status.hasRoom = function() {
+  return Session.get('room');
+};
+
+Template.status.canPlay = function() {
+  return Session.get('play');
+};
+
+Template.status.events({
+  "click .quit": function(event) {
+    var room = Session.get('room');
+    resetSession(room);
+    GameStream.emit('cancel', room);
+    event.preventDefault();
+  }
+});
+
 Template.game.events({
   "click .col": function(event) {
     if(Session.get('user')) {
-      var weapon = Session.get('weapon');
-      var col = $(event.target);
-      var row = col.closest('.row');
-      console.log("Weapon: %s | Row: %s | Col: %s", weapon, row.data('row'), col);
+      if(Session.get('room')) {
+        if(Session.get('play')) {
+          var room = Session.get('room');
+          var weapon = Session.get('weapon');
+          var col = $(event.target).data('col');
+          var row = $(event.target).closest('.row').data('row');
+          Session.set('play', false);
+          GameStream.emit('shoot', room, weapon, row, col);
+        } else {
+          alert("Aguarde a sua vez.");
+        }
+      } else {
+        alert('Escolha um inimigo primeiro.');
+      }
     } else {
-      alert("First you need to enter your name!");
+      alert('Digite seu nome primeiro.');
       $('.input').focus();
+    }
+    event.preventDefault();
+  }
+});
+
+GameStream.on('request', function(user, enemy, room) {
+  if(Session.equals('user', user)) {
+    if(confirm('Deseja jogar com '+ enemy +'?')) {
+      Session.set('enemy', enemy);
+      Session.set('weapon', WEAPON_O);
+      Session.set('room', room);
+      Session.set('play', false);
+      GameStream.emit('start', room, WEAPON_X);
+    } else {
+      resetSession(room);
+      GameStream.emit('cancel', room);
     }
   }
 });
 
-Template.game.weapon = function() {
-	return Session.get('weapon');
-};
+GameStream.on('abort', function(room) {
+  alert('Jogo cancelado.');
+  resetSession(room);
+});
+
+GameStream.on('play', function(room, weapon) {
+  if(Session.equals('room', room) && Session.equals('weapon', weapon)) {
+    Session.set('play', true);
+  }
+});
+
+GameStream.on('refresh', function(room, weapon, row, col) {
+  if(Session.equals('room', room)) {
+    var board = $('.gameboard');
+    var target = board.find('.row[data-row="'+ row +'"]');
+    target = target.find('.col[data-col="'+ col +'"]');
+    target.html('<i class="'+ weapon +'"></i>');
+    if(!Session.equals('weapon', weapon)) {
+      Session.set('play', true); 
+    }
+  }
+});
--- server/jogo-da-velha.js
+++ server/jogo-da-velha.js
@@ -1,11 +1,35 @@
 Users = new Meteor.Collection('users');
+GameStream = new Meteor.Stream('game');
+
+GameStream.permissions.write(function() { return true; });
+GameStream.permissions.read(function() { return true; });
 
 Meteor.startup(function() {
 	if(Users.find().count()) {
-		Users.remove();
+		Users.remove({});
 	}
 });
 
 Meteor.publish('onlines', function() {
 	return Users.find();
+});
+
+GameStream.on('enter', function(name) {
+	Users.insert({name: name});
+});
+
+GameStream.on('start', function(room, weapon) {
+	GameStream.emit('play', room, weapon);
+});
+
+GameStream.on('invite', function(enemy, user, room) {
+	GameStream.emit('request', enemy, user, room);
+});
+
+GameStream.on('cancel', function(room) {
+	GameStream.emit('abort', room);
+});
+
+GameStream.on('shoot', function(room, weapon, row, col) {
+	GameStream.emit('refresh', room, weapon, row, col);
 });
