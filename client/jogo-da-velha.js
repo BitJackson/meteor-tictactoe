8371ef1995ceb886265f8fdc98ce9e6a52ad7ec0
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-09-08 02:52:09 -0300

    Criando listagem de usuarios

--- client/jogo-da-velha.js
+++ client/jogo-da-velha.js
@@ -1,14 +1,41 @@
 Users = new Meteor.Collection('users');
-Meteor.subscribe('onlines');
 
-Session.set('user', 'Caio');
+Meteor.startup(function() {
+	Meteor.subscribe('onlines');
+	Session.set('weapon', 'icon-x');
+});
+
+Template.onlines.events({
+  "submit .form": function(event) {
+    var name = $(event.target).find('.input').val();
+    Session.set('user', name);
+    Users.insert({name: name});
+    event.preventDefault();
+  }
+});
 
 Template.onlines.currentUser = function() {
 	return Session.get('user');
 };
 
 Template.onlines.users = function() {
-	var users = Users.find();
-	var currentUser = Session.get('user');
-	return users;
+	return Users.find({name: {'$ne': Session.get('user')}});
+};
+
+Template.game.events({
+  "click .col": function(event) {
+    if(Session.get('user')) {
+      var weapon = Session.get('weapon');
+      var col = $(event.target);
+      var row = col.closest('.row');
+      console.log("Weapon: %s | Row: %s | Col: %s", weapon, row.data('row'), col);
+    } else {
+      alert("First you need to enter your name!");
+      $('.input').focus();
+    }
+  }
+});
+
+Template.game.weapon = function() {
+	return Session.get('weapon');
 };
--- server/jogo-da-velha.js
+++ server/jogo-da-velha.js
@@ -1,5 +1,11 @@
 Users = new Meteor.Collection('users');
 
+Meteor.startup(function() {
+	if(Users.find().count()) {
+		Users.remove();
+	}
+});
+
 Meteor.publish('onlines', function() {
 	return Users.find();
 });
