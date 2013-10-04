a9dba9f92fbdc9326977dca1195b5c4d21aa7426
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-10-04 16:24:52 -0300

    Simplificando template de ranking

--- client/lib/helpers/ranking.js
+++ client/lib/helpers/ranking.js
@@ -3,15 +3,3 @@ Template.ranking.helpers({
     return Users.find();
   },
 });
-
-Template.ranking.events({
-	"click .show": function(event) {
-		$(event.target).hide();
-		$('.ranking').removeClass('is-hide');
-	},
-	"click .close": function(event) {
-		var ranking = $(event.target).closest('.ranking');
-		ranking.addClass('is-hide');
-		$('.show').show();
-	}
-});
