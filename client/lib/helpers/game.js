0c8ff3dc934e341a2c344c928db79708be549557
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-10-29 10:31:55 -0200

    Add touchstart event for ipad shoot

--- client/lib/helpers/game.js
+++ client/lib/helpers/game.js
@@ -4,18 +4,21 @@ Template.game.helpers({
   }
 });
 
-Template.game.events({
-  "click .col": function(event) {
-    if(Session.get('play')) {
-      var room = Session.get('room');
-      var weapon = Session.get('weapon');
-      var col = $(event.target).data('col');
-      var row = $(event.target).closest('.row').data('row');
-      Session.set('play', false);
-      GameStream.emit('shoot', room, weapon, row, col);
-    } else {
-      alert("Please, wait your time.");
-    }
-    event.preventDefault();
+var playAction = function(event) {
+  if(Session.get('play')) {
+    var room = Session.get('room');
+    var weapon = Session.get('weapon');
+    var col = $(event.target).data('col');
+    var row = $(event.target).closest('.row').data('row');
+    Session.set('play', false);
+    GameStream.emit('shoot', room, weapon, row, col);
+  } else {
+    alert("Please, wait your time.");
   }
+  event.preventDefault();
+}
+
+Template.game.events({
+  "click .col": playAction,
+  "touchstart .col": playAction
 });
